43b105664b438163a231d74eb74413545baae02c
Charan91120 <ct22aar@herts.ac.uk>
2022-12-02 11:23:10 +0000

    second commit to code

--- testing_code.py
+++ testing_code.py
@@ -8,59 +8,129 @@ Created on Wed Nov 30 20:34:26 2022
 import pandas as pd
 import matplotlib.pyplot as plt
 
-read_data = pd.read_csv("CO2 emission(metric ton per_capita).csv")
+read_data_co2 = pd.read_csv("CO2 emission(metric ton per_capita).csv")
 # CO2 emission(metric ton per_capita)    electric_power_consumption(per capita)
 #print(read_data)
 #print("\n", read_data.columns)
-read_data = read_data.drop(columns=['Series Name','Series Code','Country Code',])
-print("\n", read_data.columns)
+read_data_co2 = read_data_co2.drop(columns=['Series Name','Series Code','Country Code',])
+#print("\n", read_data_co2.columns)
 
 
-read_data.columns = ['Country Name', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
+
+read_data_electric = pd.read_csv("electric_power_consumption(per capita).csv")
+print(read_data_electric)
+read_data_electric = read_data_electric.drop(columns=['Series Name','Series Code','Country Code',])
+print("\n", read_data_electric.columns)
+
+
+read_data_electric['2015 [YR2015]'] = pd.to_numeric(read_data_electric['2015 [YR2015]'],errors='coerce')
+read_data_electric['2015 [YR2015]'] = read_data_electric['2015 [YR2015]'].fillna(0)
+print(read_data_electric['2015 [YR2015]'])
+
+
+
+read_data_co2.columns = ['Country Name', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
               '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+read_data_electric.columns = ['Country Name', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008',
+              '2009', '2010', '2011', '2012', '2013', '2014', '2015']
+print(read_data_electric)
 
 # print("\n \n",read_data['Series Name'].unique())
 
+
+
+
+'''
 # remove all NaNs contain in rows..!
-read_data = read_data.dropna(axis = 0).reset_index(drop=True)
+read_data_co2 = read_data_co2.dropna(axis = 0).reset_index(drop=True)
 #print(read_data)
+read_data_electric = read_data_electric.dropna(axis = 0).reset_index(drop=True)
+print(read_data_electric)
+'''
+
+
 
 # transpose the data set
-df_t = pd.DataFrame.transpose(read_data)
-print(df_t)
+df_t_co2 = pd.DataFrame.transpose(read_data_co2)
+df_t_electric = pd.DataFrame.transpose(read_data_electric)
+print(df_t_electric)
+
+
 
 # removing un-necessary columns in data set
-new_header = df_t.iloc[0].values.tolist()
-df_t.columns = new_header
-print(df_t.columns)
+new_header = df_t_co2.iloc[0].values.tolist()
+df_t_co2.columns = new_header
+#print(df_t.columns)
+
+# electric_emission
+new_header = df_t_electric.iloc[0].values.tolist()
+df_t_electric.columns = new_header
+print(df_t_electric)
+
+
 
 # removing unwanted text in first line "Country Name row"
-df_t = df_t.iloc[1:]
-print("\n \n",df_t)
+df_t_co2 = df_t_co2.iloc[1:]
+#print("\n \n",df_t)
+df_t_electric = df_t_electric.iloc[1:]
+print("\n \n",df_t_electric)
 
-# check and remove the if null values for columns neeed
-df_t = df_t[df_t['United Kingdom'].notna()]
-df_t = df_t[df_t['Russian Federation'].notna()]
-df_t = df_t[df_t['Japan'].notna()]
-df_t = df_t[df_t['India'].notna()]
-df_t = df_t[df_t['France'].notna()]
 
-# convert data type from object to numeric by pd.numeric
-df_t['United Kingdom'] = pd.to_numeric(df_t['United Kingdom'])
-df_t['Russian Federation'] = pd.to_numeric(df_t['Russian Federation'])
-df_t['Japan'] = pd.to_numeric(df_t['Japan'])
-df_t['India'] = pd.to_numeric(df_t['India'])
-df_t['France'] = pd.to_numeric(df_t['France'])
+
+# check and remove the if null values for columns neeed in CO2-emission dataset
+df_t_co2 = df_t_co2[df_t_co2['United Kingdom'].notna()]
+df_t_co2 = df_t_co2[df_t_co2['Russian Federation'].notna()]
+df_t_co2 = df_t_co2[df_t_co2['Japan'].notna()]
+df_t_co2 = df_t_co2[df_t_co2['India'].notna()]
+df_t_co2 = df_t_co2[df_t_co2['France'].notna()]
+
+
+
+
+# convert data type from object to numeric by pd.to_numeric()
+df_t_co2['United Kingdom'] = pd.to_numeric(df_t_co2['United Kingdom'])
+df_t_co2['Russian Federation'] = pd.to_numeric(df_t_co2['Russian Federation'])
+df_t_co2['Japan'] = pd.to_numeric(df_t_co2['Japan'])
+df_t_co2['India'] = pd.to_numeric(df_t_co2['India'])
+df_t_co2['France'] = pd.to_numeric(df_t_co2['France'])
+
+
+#convert the below datatype to numeric through pd.to_numeric()
+df_t_electric['United Kingdom'] = pd.to_numeric(df_t_electric['United Kingdom'])
+df_t_electric['Russian Federation'] = pd.to_numeric(df_t_electric['Russian Federation'])
+df_t_electric['Japan'] = pd.to_numeric(df_t_electric['Japan'])
+df_t_electric['India'] = pd.to_numeric(df_t_electric['India'])
+df_t_electric['France'] = pd.to_numeric(df_t_electric['France'])
+
+
+
 
 # convert index into numeric by pd.to_numeric
-df_t_index = pd.to_numeric(df_t.index)
+df_t_index = pd.to_numeric(df_t_co2.index)
 #print("\n", df_t_index)
 
-plt.plot(df_t_index, df_t['United Kingdom'], label = 'UK')
-plt.plot(df_t_index, df_t['Russian Federation'], label = 'Russian')
-plt.plot(df_t_index, df_t['Japan'], label = 'Japan')
-plt.plot(df_t_index, df_t['India'], label = 'India')
-plt.plot(df_t_index, df_t['France'], label = 'France')
+#convert index into numeric through pd.to_numeric()
+df_t_index_electric = pd.to_numeric(df_t_electric.index)
+print("\n ", df_t_electric)
+
+
+# line plot for CO2 emission dataset for select 5 countries(2000-2015)
+plt.plot(df_t_index, df_t_co2['United Kingdom'], label = 'UK')
+plt.plot(df_t_index, df_t_co2['Russian Federation'], label = 'Russian')
+plt.plot(df_t_index, df_t_co2['Japan'], label = 'Japan')
+plt.plot(df_t_index, df_t_co2['India'], label = 'India')
+plt.plot(df_t_index, df_t_co2['France'], label = 'France')
+plt.legend()
+plt.xlabel("Years")
+plt.ylabel("Countries")
+plt.show()
+
+# line plot for electric power consumption for selected 5 countries(2000-2015)
+plt.plot(df_t_index_electric, df_t_electric['United Kingdom'], label = 'UK')
+plt.plot(df_t_index_electric, df_t_electric['Russian Federation'], label = 'Russian')
+plt.plot(df_t_index_electric, df_t_electric['Japan'], label = 'Japan')
+plt.plot(df_t_index_electric, df_t_electric['India'], label = 'India')
+plt.plot(df_t_index_electric, df_t_electric['France'], label = 'France')
 plt.legend()
 plt.xlabel("Years")
 plt.ylabel("Countries")
@@ -69,7 +139,12 @@ plt.show()
 
 #df_t['United Kingdom'].plot.bar(df_t_index)
 #df_t.plot.bar(df_t_index, ['United Kingdom', 'Japan'])
-df_t.plot( y = ['United Kingdom', 'Russian Federation','Japan', 'India', 'France'], kind = 'bar', figsize = (15,5))
+df_t_co2.plot( y = ['United Kingdom', 'Russian Federation','Japan', 'India', 'France'], kind = 'bar', figsize = (10,5))
 plt.xlabel("Year")
 plt.ylabel("Countries")
+plt.show()
+
+
+#bar grapth representing countries from 2000 to 2015
+df_t_electric.plot(y = ['United Kingdom', 'Russian Federation','Japan', 'India', 'France'], kind = 'bar', figsize = (10,5))
 plt.show()
